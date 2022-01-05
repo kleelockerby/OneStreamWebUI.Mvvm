71123bcb97aeb6d3fb649866b7af2181c7fa4654
Kevin Lockerby <klockerby@onestreamsoftware.com>
2022-01-05 16:30:46 -0500

    Fixed Service read from json file

--- OneStreamMvvmBlazor/Client/Services/WeatherForecastGetterJson.cs
+++ OneStreamMvvmBlazor/Client/Services/WeatherForecastGetterJson.cs
@@ -1,40 +1,23 @@
-﻿using System.IO;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using OneStreamMvvmBlazor.Shared;
-using Newtonsoft.Json;
 
 namespace OneStreamMvvmBlazor.Client
 {
     public class WeatherForecastGetterJson : IWeatherForecastGetter
     {
-       private  string path = "weatherForecast.json";
+        private readonly HttpClient _httpClient;
+
+        public WeatherForecastGetterJson(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
         public Task<IEnumerable<WeatherForecastEntity>?> GetForecasts()
         {
-            //using (StreamReader r = new StreamReader(@"C:\SourceFiles\weather3.json"))
-            try
-            {
-                Console.WriteLine(Environment.CurrentDirectory);
-
-                /*using (StreamReader r = new StreamReader(path))
-                {
-                    string json = r.ReadToEnd();
-                    //IEnumerable<WeatherForecastEntity>? forecasts = JsonConvert.DeserializeObject<IEnumerable<WeatherForecastEntity>>(json);
-                    //return Task.FromResult <WeatherForecastEntity> forecasts;
-
-                    return (Task<IEnumerable<WeatherForecastEntity>?>)Task.Run(() =>
-                    {
-                        IEnumerable<WeatherForecastEntity>? forecasts = JsonConvert.DeserializeObject<IEnumerable<WeatherForecastEntity>>(json);
-                    });
-                }*/
-                return null;
-            }
-            catch (Exception ex)
-            {
-                string fuckYouMS = ex.Message;
-                throw;
-            }
+            return _httpClient.GetFromJsonAsync<IEnumerable<WeatherForecastEntity>>("sample-data/weatherFOrecast.json");
         }
     }
 }
